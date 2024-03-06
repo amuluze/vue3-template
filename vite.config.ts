59f065eb2432dca92542e5ac71fbb75bc8be7ed3
jialong.wang <jialong.wang@chaitin.com>
2024-03-06 15:07:13 +0800

    feat:first commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 9000,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path: string) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+});
