64400101f7857fb4d5cb29537f3674af32b60602
Amu <wangjialong89@yeah.net>
2024-11-10 00:47:19 +0800

    feat: update

---
+++ src/layout/navbar/Breadcrumb.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const router = useRouter()
+
+const breadcrumbList = computed(() => {
+  return router.currentRoute.value.matched.map(item => item)
+})
+</script>
+
+<template>
+    <div class="am-breadcrumb">
+        <svg-icon icon-class="homepage" />
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index" :to="{ path: item.path }">
+                <span>{{ item.meta.title }}</span>
+            </el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@include b(breadcrumb) {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+}
+</style>
---
+++ src/views/user/role/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>role</div>
+</template>
+
+<style scoped lang="scss"></style>
